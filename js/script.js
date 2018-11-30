69a01091819a5d655095e87c0aaa901245d1be15
Duncan Kariuki <duncankariuki60@gmail.com>
2018-11-30 16:28:19 +0300

    added a form filled

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function triangleType() {
+function triangleType(result) {
   // triangleType(sides);
   var sides = [];
   for (var i = 0; i <= 2; i++) {
-    sides.push(parseInt(prompt('Enter the value of triangle sides ' + (i + 1))));
+    sides.push(parseInt(
+
+			prompt('Enter the value of triangle sides ' + (i + 1))));
     // sides = sides.sort((a, b) => a - b);
 
   }
@@ -10,17 +12,23 @@ function triangleType() {
   var b = Math.pow(sides[1], 2);
   var c = Math.pow(sides[2], 2);
 
-  if (c === a + b) {
-    console.log("right triangle");
+	if (c === a + b) {
+    //console.log("right triangle");
+		return "right triangle";
   } else if (c < a + b) {
-    console.log("acute triangle");
+    //console.log("acute triangle");
+		return "acute triangle";
   } else if (a === b, b !== c) {
-    console.log("isosceles triangle");
+    //console.log("isosceles triangle");
+		return "isosceles triangle";
   } else if (a === b, b === c) {
-    console.log("equilateral triangle");
+    //console.log("equilateral triangle");
+		return "equilateral triangle";
   } else if (a != b, b !== c) {
-    console.log("scalene triangle");
+    //console.log("scalene triangle");
+		return "scalene triangle";
   } else {
-    console.log("not a triangle");
+    //console.log("not a triangle");
+		return "right not a triangle";
   }
 }
