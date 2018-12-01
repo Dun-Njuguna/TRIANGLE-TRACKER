40d7ac583050582f0946ef0e4dec499399115397
Duncan Kariuki <duncankariuki60@gmail.com>
2018-12-01 21:07:31 +0300

    changed css styles and added more control flows on javascript file

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ function triangleType() {
   var sides = [];
   for (var i = 0; i <= 2; i++) {
     sides.push(parseInt(
-
       prompt('Enter the value of triangle sides ' + (i + 1))));
-
   }
   var a = Math.pow(sides[0], 2);
   var b = Math.pow(sides[1], 2);
   var c = Math.pow(sides[2], 2);
-	var result;
+  var result;
   if (c === a + b) {
     result = "right triangle";
     console.log(result);
