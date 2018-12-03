002bac232cd8ae1de7e88f76f37bde43a9441f82
Duncan Kariuki <duncankariuki60@gmail.com>
2018-12-03 11:08:11 +0300

    used parseint

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
 function triangleType() {
-  var sides = [];
   var one = document.getElementById('first').value;
   var two = document.getElementById('second').value;
   var three = document.getElementById('third').value;
+  var sides = [];
   sides.push(one, two, three);
-
-  var a = Math.pow(sides[0], 2);
-  var b = Math.pow(sides[1], 2);
-  var c = Math.pow(sides[2], 2);
+  var a = parseInt(sides[0]);
+  var b = parseInt(sides[1]);
+  var c = parseInt(sides[2]);
   var result;
-  if (a===b, b===c, c===a) {
+  if (a === b, b === c, c === a) {
     result = "Equilateral triangle";
     console.log(result);
     document.getElementById("myText").innerHTML = result;
-  }
-  else if (a===b || c===b || c===a) {
+  } else if (a === b || c === b || c === a) {
     result = "Isosceles triangle";
     console.log(result);
     document.getElementById("myText").innerHTML = result;
-  }
-  else if (a!==b && b!==c && a!==c) {
-    result = "Scalene triangle";
-    console.log(result);
-    document.getElementById("myText").innerHTML = result;
-  } else {
+  } else if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a)) {
     result = "not a triangle";
     console.log(result);
     document.getElementById("myText").innerHTML = result;
+  } else {
+    result = "Scalene";
+    console.log(result);
+    document.getElementById("myText").innerHTML = result;
   }
-
 }
